223e7604cbdb6fea3f868409fb43f49b261bca23
Sergiu Luca <sergiu.luca@insuretech.ro>
2023-10-06 22:12:02 +0300

    temporary fix db not found dns

--- src/pages/api/ratings.ts
+++ src/pages/api/ratings.ts
@@ -17,34 +17,34 @@ export default async function updateCrowdingLevel({
         } | null>
     >;
 }) {
-    const client = await clientPromise;
-    const db = client.db("car-chargers");
+    // const client = await clientPromise;
+    // const db = client.db("car-chargers");
 
     // Calculate the new crowding level
     const newLevel = Math.round((currentValue + newValue) / 2);
 
     try {
         // Update the crowding level in the database
-        const result = await db.collection("car-chargers-info").updateOne(
-            { _id },
-            {
-                $set: {
-                    crowding_level: newLevel,
-                },
-            }
-        );
+        // const result = await db.collection("car-chargers-info").updateOne(
+        //     { _id },
+        //     {
+        //         $set: {
+        //             crowding_level: newLevel,
+        //         },
+        //     }
+        // );
 
-        if (result.modifiedCount > 0) {
-            setAlertOptions({
-                text: "Nivelul de aglomeratie a fost actualizat cu succes!",
-                type: "success",
-            });
-        } else {
-            setAlertOptions({
-                text: "Nivelul de aglomeratie nu a putut fi actualizat cu succes!",
-                type: "error",
-            });
-        }
+        // if (result.modifiedCount > 0) {
+        //     setAlertOptions({
+        //         text: "Nivelul de aglomeratie a fost actualizat cu succes!",
+        //         type: "success",
+        //     });
+        // } else {
+        //     setAlertOptions({
+        //         text: "Nivelul de aglomeratie nu a putut fi actualizat cu succes!",
+        //         type: "error",
+        //     });
+        // }
     } catch (error) {
         console.error("Error updating crowding level:", error);
         setAlertOptions({
